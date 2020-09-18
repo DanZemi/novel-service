6b75ba3d20d862309d9f5f67b809d878fcbea929
山根大輝 <12183597@g.matsuyama-u.ac.jp>
2020-09-19 00:00:01 +0900

    :update

--- lib/novel_service/accounts/user.ex
+++ lib/novel_service/accounts/user.ex
@@ -9,24 +9,23 @@ defmodule NovelService.Accounts.User do
     field :name, :string
     field :password, :string
     field :have_articles, :integer, default: 0
+    field :bio, :string, default: "よろしくお願いします。"
+    field :user_link, :string
     has_many :articles, Article
-
-
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password])
+    |> cast(attrs, [:name, :email, :password, :bio, :user_link])
     |> validate_required([:name, :email, :password], message: "空白になっています")
     |> validate_length(:name, min: 6, message: "6文字以上にして下さい")
-    |> validate_format(:name, ~r/^[a-zA-Z0-9_-]$/, message: "使用できない文字が含まれています")
+    #|> validate_format(:name, ~r/^[a-zA-Z0-9_-]$/, message: "使用できない文字が含まれています")
     |> unique_constraint(:email)
     |> validate_format(:email, ~r/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "メールアドレスの形式が間違っています")
     |> validate_format(:password, ~r/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}$/i, message: "半角英数字それぞれ1文字以上含む8文字以上100文字以下の文字にして下さい")
     |> validate_confirmation(:password, message: "パスワードと一致しません")
-    |> IO.inspect
     |> put_pass_hash()
   end
 
