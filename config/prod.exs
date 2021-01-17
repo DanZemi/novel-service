42d92cd2f9fe05d53997051f0d8afb3845c19141
daikiyamane <12183597@g.matsuyama-u.ac.jp>
2021-01-18 01:08:25 +0900

    New

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :novel_service, NovelServiceWeb.Endpoint,
   http: [port: 4000],
-  check_origin: ["//https://branchnovels.gigalixirapp.com"]
+  check_origin: ["https://branchnovels.gigalixirapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
