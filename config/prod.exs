78b31701faac984fc22592ce13480ea21c149095
daikiyamane <12183597@g.matsuyama-u.ac.jp>
2021-01-18 02:12:58 +0900

    Update:config

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :novel_service, NovelServiceWeb.Endpoint,
+  http: [port: 4000],
   url: [scheme: "https", host: "https://branchnovels.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
