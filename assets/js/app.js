78a6ac605714e6b898dd7d08731192f826a8ffd7
daikiyamane <12183597@g.matsuyama-u.ac.jp>
2021-02-22 00:59:37 +0900

    fix js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
 import "bootstrap"
+import "../css/app.scss"
 
 
 // webpack automatically bundles all modules in your
